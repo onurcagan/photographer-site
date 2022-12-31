76957f7a23fdc7a30ab2f0e6935b3c8e222fcb6e
Resuspott <onurcagann@gmail.com>
2022-12-31 14:57:13 +0300

    init commit

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import { Navbar } from '../src/components/Navbar'
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <div className='flex h-screen mb-12 bg-fixed bg-center bg-cover bg-black/70 custom-img'></div>
+    </>
+  )
+}
