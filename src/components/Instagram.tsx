28778b01843cd786151db75080c2b7c4421a4d16
Onur Çağan <onurcagann@gmail.com>
2023-01-05 19:30:44 +0300

    adjusted values for better responsive design

--- src/components/Instagram.tsx
+++ src/components/Instagram.tsx
@@ -5,15 +5,15 @@ import InstagramImg from './InstagramImg'
 
 const Instagram = () => {
   return (
-    <div className="max-w-[1440px] flex flex-col md:h-screen items-center justify-center mx-auto text-center pb-2 md:pb-8 lg:pb-16 pt-24 lg:mt-32 px-2 text-gray-300">
+    <div className="max-w-[1440px] flex flex-col md:h-screen items-center justify-center mx-auto text-center pb-2 md:pb-8 lg:pb-16 pt-12 lg:mt-32 px-2 text-gray-300">
       <p className="text-2xl font-bold">Follow me on Instagram</p>
       <div className="flex flex-row items-center justify-center">
         <FaInstagram />
         <p className="pl-1">Blanc Photography</p>
       </div>
       {/* Spacer */}
-      <div className="pb-6" />
-      <div className="grid grid-cols-2 gap-2 p-2 md:grid-cols-3 ">
+      <div className="pb-6 md:pb-12" />
+      <div className="grid grid-cols-2 gap-2 md:px-48 md:grid-cols-3">
         {InstagramData.map((e, i) => {
           return <InstagramImg key={i} socialImg={e.image} />
         })}
