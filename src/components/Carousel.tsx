d9a685fdc2f8516646cede91ede90845c0841241
Onur Çağan <onurcagann@gmail.com>
2023-01-05 19:29:17 +0300

    redesigned carousel to allow swiping on mobile

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,52 +1,38 @@
-import Image from 'next/image'
-import { useState } from 'react'
-import { FaArrowCircleLeft, FaArrowCircleRight } from 'react-icons/fa'
-import { RxDotFilled } from 'react-icons/rx'
-import { MdOutlineArrowBackIos, MdOutlineArrowForwardIos } from 'react-icons/md'
+/* eslint-disable @next/next/no-img-element */
+import { AiOutlineLeftCircle, AiOutlineRightCircle } from 'react-icons/ai'
+import { slideLeft, slideRight } from '../helper/sliderFunctions'
 
 export function Carousel({ slides }: { slides: { image: string }[] }) {
-  const [currentIndex, setCurrentIndex] = useState(0)
-
-  const prevSlide = () => {
-    const isFirstSlide = currentIndex === 0
-    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1
-    setCurrentIndex(newIndex)
-  }
-
-  const nextSlide = () => {
-    const isLastSlide = currentIndex === slides.length - 1
-    const newIndex = isLastSlide ? 0 : currentIndex + 1
-    setCurrentIndex(newIndex)
-  }
-
-  const goToSlide = (slideIndex: number) => {
-    setCurrentIndex(slideIndex)
-  }
-
   return (
-    <div id="gallery" className="flex flex-col items-center justify-center h-screen">
-      <div className="pt-[100px]" />
-      <h1 className="text-4xl text-center text-white">Gallery</h1>
-      <div className="max-w-[1440px] h-[780px] w-full m-auto pt-12 pb-16 px-4 relative group">
-        <div
-          style={{ backgroundImage: `url(${slides[currentIndex].image})` }}
-          className="w-full h-full duration-500 bg-center bg-cover rounded-2xl"
-        ></div>
-        {/* Left Arrow */}
-        <div className="block md:hidden md:group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/40 text-white cursor-pointer">
-          <MdOutlineArrowBackIos onClick={prevSlide} size={25} />
-        </div>
-        {/* Right Arrow */}
-        <div className="block md:hidden md:group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/40 text-white cursor-pointer">
-          <MdOutlineArrowForwardIos onClick={nextSlide} size={25} />
-        </div>
-        <div className="flex justify-center py-2 top-4">
-          {slides.map((s, slideIndex) => (
-            <div key={slideIndex} onClick={() => goToSlide(slideIndex)} className="text-2xl cursor-pointer">
-              <RxDotFilled color="white" />
-            </div>
-          ))}
-        </div>
+    <div
+      id="gallery"
+      className="relative items-center justify-center py-16 text-center text-gray-300 md:flex md:flex-col md:min-h-screen md:px-4 sm:px-16"
+    >
+      <h1 className="text-4xl animate-wiggle">Gallery</h1>
+      <div className="py-4" />
+
+      <AiOutlineRightCircle
+        className="absolute top-[50%] z-10 text-4xl md:text-5xl text-white -translate-y-[-50%] md:bg-gray-800/40 right-3 md:right-12 rounded-full p-1 cursor-pointer"
+        onClick={() => slideRight(slides)}
+      />
+      <div
+        id="slider"
+        className="my-auto gap-2 md:gap-12 flex w-full h-full overflow-x-scroll scroll-smooth snap-x snap-mandatory before:shrink-0 before:w-[30vw] after:shrink-0 after:w-[30vw] no-scrollbar"
+      >
+        <AiOutlineLeftCircle
+          className="absolute top-[50%] z-10 text-4xl md:text-5xl text-white -translate-y-[-50%] md:bg-gray-800/40 left-3 md:left-12 rounded-full p-1 cursor-pointer"
+          onClick={() => slideLeft(slides)}
+        />
+        {slides.map((slide, index) => {
+          return (
+            <img
+              className=" w-[85%] md:w-[60%] p-2 duration-500 ease-in-out cursor-pointer hover:opacity-90 snap-center select-none"
+              key={index}
+              src={slide.image}
+              alt=""
+            />
+          )
+        })}
       </div>
     </div>
   )
