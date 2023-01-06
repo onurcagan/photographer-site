4b4bb0b3bc7988fc948c3d0eccdb3e122a458a0e
Onur Çağan <onurcagann@gmail.com>
2023-01-06 05:39:52 +0300

    ios bug test #4

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,8 +4,8 @@ import { handleClick } from '../helper/HeroClickHandler'
 function Hero({ heading, message, btn }: { heading: string; message: string; btn: string }) {
   const router = useRouter()
   return (
-    <div className="flex items-center justify-center min-h-screen bg-center bg-cover custom-img">
-      <div className="absolute top-0 bottom-0 left-0 right-0 bg-black/70 z-[2]" />
+    <div className="flex items-center justify-center min-h-screen bg-fixed bg-center bg-cover custom-img">
+      <div className="absolute h-full w-full bg-black/70 z-[2]" />
       <div className="p-5 text-white z-[2] -mt-12">
         <h2 className="text-4xl font-bold sm:text-5xl">{heading}</h2>
         <p className="py-5 sm:text-xl">{message}</p>
