0a51b1a40c81ebf166a5c683d1ca15a7ca5e288f
Onur Çağan <onurcagann@gmail.com>
2023-01-06 06:02:27 +0300

    reworked contact page to better fit mobile screens

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,10 +4,10 @@ const Contact = () => {
   return (
     <div
       id="contact"
-      className="flex flex-col items-center justify-center min-h-screen p-4 m-auto text-black bg-white bg-cover contact-img"
+      className="flex flex-col items-center justify-center min-h-screen p-4 pt-56 m-auto text-black bg-white bg-cover sm:pt-0 contact-img"
     >
-      <h1 className="p-4 text-4xl font-bold text-center">Let&apos;s work together.</h1>
-      <div className="my-8" />
+      <h1 className="p-4 text-4xl font-bold text-center sm:pl-0">Let&apos;s work together.</h1>
+      <div className="my-2" />
       <form className="max-w-[600px] mx-auto">
         <div className="grid grid-cols-2 gap-2">
           <input className="p-3 border rounded-md shadow-lg" placeholder="Name"></input>
