404eacb5fb3dbc670f074af9a57f6fa69c0cba97
Onur Çağan <onurcagann@gmail.com>
2023-01-06 04:12:31 +0300

    created contact route

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Contact from '../src/components/Contact'
 import Hero from '../src/components/Hero'
 
 const Index = () => {
   return (
     <>
       <Hero heading="Contact" message="Submit the form below for more work and quotes!" btn="Scroll Down" />
+      <Contact />
     </>
   )
 }
