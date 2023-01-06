4a3904d2faf078f8fcac2e8e32ded3b29eaceb44
Onur Çağan <onurcagann@gmail.com>
2023-01-06 05:42:07 +0300

    ios bug test #5

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ function Hero({ heading, message, btn }: { heading: string; message: string; btn
   const router = useRouter()
   return (
     <div className="flex items-center justify-center min-h-screen bg-fixed bg-center bg-cover custom-img">
-      <div className="absolute h-full w-full bg-black/70 z-[2]" />
+      <div className="absolute h-screen w-full bg-black/70 z-[2]" />
       <div className="p-5 text-white z-[2] -mt-12">
         <h2 className="text-4xl font-bold sm:text-5xl">{heading}</h2>
         <p className="py-5 sm:text-xl">{message}</p>
