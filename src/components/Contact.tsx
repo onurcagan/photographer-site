a20d175cc8be5843951d0526345ccc0a2387cd0d
Onur Çağan <onurcagann@gmail.com>
2023-01-13 23:59:19 +0300

    updated the contact form onClick fn

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,21 +3,32 @@ import React from 'react'
 const Contact = () => {
   return (
     <div
-      id="contact"
-      className="flex flex-col items-center justify-center min-h-screen p-4 pt-56 m-auto text-black bg-white bg-cover sm:pt-0 contact-img"
+      id='contact'
+      className='flex flex-col items-center justify-center min-h-screen p-4 pt-56 m-auto text-black bg-white bg-cover sm:pt-0 contact-img'
     >
-      <h1 className="p-4 text-4xl font-bold text-center sm:pl-0">Let&apos;s work together.</h1>
-      <div className="my-2" />
-      <form className="max-w-[600px] mx-auto">
-        <div className="grid grid-cols-2 gap-2">
-          <input className="p-3 border rounded-md shadow-lg" placeholder="Name"></input>
-          <input className="p-3 border rounded-md shadow-lg" placeholder="Email"></input>
+      <h1 className='p-4 text-4xl font-bold text-center sm:pl-0'>Let&apos;s work together.</h1>
+      <div className='my-2' />
+      <form className='max-w-[600px] mx-auto'>
+        <div className='grid grid-cols-2 gap-2'>
+          <input className='p-3 border rounded-md shadow-lg' placeholder='Name'></input>
+          <input className='p-3 border rounded-md shadow-lg' placeholder='Email'></input>
         </div>
-        <input placeholder="Subject" className="w-full p-3 my-2 border rounded-md shadow-lg" />
-        <textarea className="w-full p-3 border rounded-md shadow-lg" placeholder="Message" rows={10} cols={30} />
+        <input placeholder='Subject' className='w-full p-3 my-2 border rounded-md shadow-lg' />
+        <textarea
+          className='w-full p-3 border rounded-md shadow-lg'
+          placeholder='Message'
+          rows={10}
+          cols={30}
+        />
         <button
-          className="w-full p-3 font-semibold bg-white border rounded-md shadow-lg"
-          onClick={() => alert('You can e-mail me at onurcagann@gmail.com!')}
+          className='w-full p-3 font-semibold bg-white border rounded-md shadow-lg'
+          onClick={() => {
+            window.open(
+              "mailto:onurcagann@gmail.com?subject=We'd Like To Work With You!&body=Hey Onur. This is who we are, If you'd like to work with us we'd love to work with you!",
+            )
+            alert("Why don't you e-mail me as well?")
+            return
+          }}
         >
           Submit
         </button>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -25,45 +25,66 @@ export const Navbar = () => {
 
   return (
     <nav
-      id="navbar"
-      className="fixed top-0 left-0 z-10 w-full px-4 py-4 duration-300 ease-in border-gray-200 sm:px-6"
+      id='navbar'
+      className='fixed top-0 left-0 z-10 w-full px-4 py-4 duration-300 ease-in border-gray-200 sm:px-6'
       style={{ backgroundColor: `${bgColor}` }}
     >
       {/* Main Div */}
-      <div className="container flex flex-wrap items-center justify-between mx-auto">
-        <Link href="/" className="flex items-center">
-          <span className="self-center text-xl font-semibold whitespace-nowrap" style={{ color: `${textColor}` }}>
+      <div className='container flex flex-wrap items-center justify-between mx-auto'>
+        <Link href='/' className='flex items-center'>
+          <span
+            className='self-center text-xl font-semibold whitespace-nowrap'
+            style={{ color: `${textColor}` }}
+          >
             C. Rick Blanc
           </span>
         </Link>
         <button
-          data-collapse-toggle="navbar-default"
-          type="button"
-          className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          aria-controls="navbar-default"
-          aria-expanded="false"
+          data-collapse-toggle='navbar-default'
+          type='button'
+          className='inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600'
+          aria-controls='navbar-default'
+          aria-expanded='false'
           onClick={() => setNav(true)}
           style={{ color: `${textColor}` }}
         >
-          <span className="sr-only">Open main menu</span>
-          <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-            <path d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"></path>
+          <span className='sr-only'>Open main menu</span>
+          <svg
+            className='w-6 h-6'
+            aria-hidden='true'
+            fill='currentColor'
+            viewBox='0 0 20 20'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path d='M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z'></path>
           </svg>
         </button>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+        <div className='hidden w-full md:block md:w-auto' id='navbar-default'>
           <ul
             style={{ color: `${textColor}` }}
             className={`flex flex-col p-4 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0`}
           >
-            <li className={`${bgColor === 'transparent' ? 'hover:text-slate-200' : 'hover:text-black/60'}`}>
-              <Link href="/" className="block py-2 pl-3 pr-4 md:hover:bg-transparent md:border-0 md:p-0" aria-current="page">
+            <li
+              className={`${
+                bgColor === 'transparent' ? 'hover:text-slate-200' : 'hover:text-black/60'
+              }`}
+            >
+              <Link
+                href='/'
+                className='block py-2 pl-3 pr-4 md:hover:bg-transparent md:border-0 md:p-0'
+                aria-current='page'
+              >
                 Home
               </Link>
             </li>
-            <li className={`${bgColor === 'transparent' ? 'hover:text-slate-200' : 'hover:text-black/60'}`}>
+            <li
+              className={`${
+                bgColor === 'transparent' ? 'hover:text-slate-200' : 'hover:text-black/60'
+              }`}
+            >
               <Link
-                href="/#gallery"
-                className="block py-2 pl-3 pr-4 md:hover:bg-transparent md:border-0 md:p-0"
+                href='/#gallery'
+                className='block py-2 pl-3 pr-4 md:hover:bg-transparent md:border-0 md:p-0'
                 onClick={() => {
                   let navbar = document.getElementById('navbar')
                   navbar!.style.opacity = '0'
@@ -73,13 +94,27 @@ export const Navbar = () => {
                 Gallery
               </Link>
             </li>
-            <li className={`${bgColor === 'transparent' ? 'hover:text-slate-200' : 'hover:text-black/60'}`}>
-              <Link href="/work" className="block py-2 pl-3 pr-4 md:hover:bg-transparent md:border-0 md:p-0">
+            <li
+              className={`${
+                bgColor === 'transparent' ? 'hover:text-slate-200' : 'hover:text-black/60'
+              }`}
+            >
+              <Link
+                href='/work'
+                className='block py-2 pl-3 pr-4 md:hover:bg-transparent md:border-0 md:p-0'
+              >
                 Work
               </Link>
             </li>
-            <li className={`${bgColor === 'transparent' ? 'hover:text-slate-200' : 'hover:text-black/60'}`}>
-              <Link href="/contact" className="block py-2 pl-3 pr-4 md:hover:bg-transparent md:border-0 md:p-0">
+            <li
+              className={`${
+                bgColor === 'transparent' ? 'hover:text-slate-200' : 'hover:text-black/60'
+              }`}
+            >
+              <Link
+                href='/contact'
+                className='block py-2 pl-3 pr-4 md:hover:bg-transparent md:border-0 md:p-0'
+              >
                 Contact
               </Link>
             </li>
@@ -91,18 +126,18 @@ export const Navbar = () => {
           className={`absolute inset-0 flex items-center justify-center w-full h-screen text-center duration-300 ease-in bg-black md:hidden ${
             nav ? 'left-0' : 'left-[-100%]'
           }`}
-          id="navbar-default"
+          id='navbar-default'
         >
-          <div className="absolute top-4 right-4 p-2.5 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 ">
-            <AiOutlineClose color="lightgray" size={'1.2rem'} onClick={() => setNav(false)} />
+          <div className='absolute top-4 right-4 p-2.5 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 '>
+            <AiOutlineClose color='lightgray' size={'1.2rem'} onClick={() => setNav(false)} />
           </div>
 
-          <ul className="flex flex-col gap-12 p-4 mt-4 text-4xl rounded-lg">
+          <ul className='flex flex-col gap-12 p-4 mt-4 text-4xl rounded-lg'>
             <li>
               <Link
-                href="/"
-                className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                aria-current="page"
+                href='/'
+                className='block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
+                aria-current='page'
                 onClick={() => setNav(false)}
               >
                 Home
@@ -110,8 +145,8 @@ export const Navbar = () => {
             </li>
             <li>
               <Link
-                href="/#gallery"
-                className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                href='/#gallery'
+                className='block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
                 onClick={() => {
                   setNav(false)
                   let navbar = document.getElementById('navbar')
@@ -124,8 +159,8 @@ export const Navbar = () => {
             </li>
             <li>
               <Link
-                href="/work"
-                className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                href='/work'
+                className='block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
                 onClick={() => setNav(false)}
               >
                 Work
@@ -133,8 +168,8 @@ export const Navbar = () => {
             </li>
             <li>
               <Link
-                href="/contact"
-                className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                href='/contact'
+                className='block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
                 onClick={() => setNav(false)}
               >
                 Contact
